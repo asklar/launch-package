f063a3bfa116f14b313d9b4a3746513401eaac6b
Alexander Sklar <asklar@microsoft.com>
2021-12-12 10:17:44 -0800

    error msg

--- bin/index.js
+++ bin/index.js
@@ -2,6 +2,10 @@
 
 const cp = require('child_process');
 const powershell = `${process.env.SystemRoot}\\System32\\WindowsPowerShell\\v1.0\\powershell.exe`;
-$out = cp.execSync(`${powershell} -c ${__dirname}\\launch.ps1 ${process.argv.slice(2)}`);
-
-console.log($out.toString());
+const args = process.argv.slice(2);
+if (args.length === 0) {
+  console.error(`Syntax: launch-package <packageSpec> [args] - where packageSpec is a package family name; use * as wildcard.`);
+  return -1;
+}
+const out = cp.execSync(`${powershell} -c ${__dirname}\\launch.ps1 ${args}`);
+console.log(out.toString());
