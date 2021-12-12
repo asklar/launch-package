f56c62fe9af6ca4d58297b225ffba46fda92b1f7
Alexander Sklar <asklar@microsoft.com>
2021-12-12 10:13:27 -0800

    >

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const cp = require('child_process');
 const powershell = `${process.env.SystemRoot}\\System32\\WindowsPowerShell\\v1.0\\powershell.exe`;
 $out = cp.execSync(`${powershell} -c ${__dirname}\\launch.ps1 ${process.argv.slice(2)}`);
